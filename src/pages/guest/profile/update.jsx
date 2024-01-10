40c68d62c80de4a59c9dbbc2158764dee46ecc75
h3nrush <hassanmohammadi078@gmail.com>
2024-01-10 04:09:17 +0100

    last_update

--- src/pages/guest/profile/update.jsx
+++ src/pages/guest/profile/update.jsx
@@ -1,11 +1,13 @@
 import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './style/style.css'
 
 function UpdateUser() {
   const [message, setMessage] = useState(null);
   const [user, setUser] = useState(null);
   const [isOwn, setIsOwn] = useState(null);
+  const [isCorrect , setIsCorrect] = useState(false);
   const token = localStorage.getItem('jwt');
   const navigat = useNavigate();
 
@@ -27,25 +29,26 @@ function UpdateUser() {
     let inputPassword2 = document.querySelector(".password2");
     let valueInput = inputPassword.value;
     let valueInput2 = inputPassword2.value;
-    let fontColor = document.querySelector(".alert-match")
+    let fontColor = document.getElementById("alertMatch");
+    if(valueInput && valueInput === valueInput2 && valueInput.length > 5){
+      setIsCorrect(true)
+    }
     if (valueInput) {
+      if(valueInput.length < 5){
+        setMessage('please choose a strong password! :_:')
+        fontColor.style.color = "red";
+        return
+      }
       if (valueInput === valueInput2) {
         setMessage('Passwords match :)');
-        fontColor.style.color = "green"
+        fontColor.style.color = "green";
       } else {
         setMessage('Passwords do not match!');
-        fontColor.style.color = "red"
+        fontColor.style.color = "red";
+        return
       }
     }
-    if (valueInput) {
-      if (valueInput.linght <= 5) {
-        setMessage('please use a strong password!');
-        fontColor.style.color = "yellow"
-      } else {
-        setMessage('wow how strong password :)');
-        fontColor.style.color = "green"
-      }
-    }
+
   };
 
   const handleUpdateUser = async (event) => {
@@ -83,11 +86,18 @@ function UpdateUser() {
       console.error("Error updating user:", error.message);
     }
   };
+  
+  const handleReset = () => {
+  document.querySelector(".password1").value = '';
+  document.querySelector(".password2").value = '';
+  }
+
 
   return (
     <div className="user-UpdateUser">
       <form onSubmit={handleUpdateUser} className="form-updateUser">
-        {message && <p className="alert-match">{message}</p>}
+
+      <p id="alertMatch" Style={!message && "color:rgb(3, 154, 255)"}>{message ? message : <>change your password with strong characters<br/>ex:<br/> <strong Style={"color:red;"}>!@#$%&* and AbCd</strong></>}</p>
         <label>
           <input
             type="password"
@@ -105,7 +115,7 @@ function UpdateUser() {
             onChange={passwordCheck}
           />
         </label>
-        <input type="submit" value="Save" className="sbt-update" />
+        {isCorrect ? (<input type="submit" value="Save" id="sbtUpdate" />):(<button id="ResetClick" onClick={handleReset}>Reset</button>)}
       </form>
     </div>
   );
